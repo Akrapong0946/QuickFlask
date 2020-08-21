009f23e147bddb29c993c44489dc98a06b3b55ed
l zh <lzhlzhlzh123@gmail.com>
2020-08-21 07:37:47 +0000

    imported chess stuff, made board update

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask import render_template, redirect, request
-from chess import WebInterface, Board
+from chess import WebInterface, Board, BasePiece
+
+def split_and_convert(inputstr):
+    '''Convert 5-char inputstr into start and end tuples.'''
+    start, end = inputstr.split(' ')
+    start = (int(start[0]), int(start[1]))
+    end = (int(end[0]), int(end[1]))
+    return (start, end)
 
 app = Flask(__name__)
 ui = WebInterface()
@@ -27,8 +34,11 @@ def play():
     # TODO: get player move from POST request object
     player_move = request.form['player_input']
     # TODO: convert player input to start & end coord
+    start, end = split_and_convert(player_move)
     # TODO: Validate move, display error msg if move is invalid
     # TODO: Update the game object and ui object
+    game.update(start, end)
+    ui.board = game.display()
     return render_template('chess.html', ui=ui)
    
 
